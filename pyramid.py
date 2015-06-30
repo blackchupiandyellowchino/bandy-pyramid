c40167f6494437c4c4aeee1887168c513738cb6b
sebamoralesasd <sebasuni96@gmail.com>
2015-06-30 17:30:55 -0300

    Prolijidad (algo)

--- pyramid.py
+++ pyramid.py
@@ -1,21 +1,3 @@
-# Realizar un sistema utilizando POO que permita resolver piramides numericas (el jueguito de La Capital)
-# Basicamente el numero de arriba es la suma de los dos de abajo
-# Ademas, debe detectarse si se puede resolver el problema o no y la base de esa piramide deben ser numeros 
-# del 1 al 6 sin repetir (Hipoteticamente esto dejo de ser condicion base)
-# La piramide debe poder ser ingresada de forma incompleta
-# Nota maxima si no se imprime la piramide: 8
-# Nota maxima si no se toman en cuenta ejercicios que no se puedan resolver: 7
-# Ejercicios con solucion unica. Si se encuentran uno o mas soluciones, faltan datos o no se puede resolver
-# No se puede usar un array para definir la piramide
-
- 
- 
- 
- 
-"""No usar tildes, python los toma como error de sintaxis"""
- 
- 
- 
 # Libreria necesaria para cortar programa
 
 import sys
